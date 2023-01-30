d0e373450ebe216f07b3e93f181720f0b345be2c
fernando <fernandoreal.valentim@gmail.com>
2023-01-30 18:34:21 -0300

    styles redone

--- pomodoro/src/App.tsx
+++ pomodoro/src/App.tsx
@@ -4,8 +4,15 @@ import { Timer } from "./components/Timer";
 function App() {
   return (
     <div className="App">
-      <section className="main">
-        <section>a</section>
+      <section className="main-container">
+        <section className="main">
+
+          <section className="upper">
+            <div className="circle"></div>
+          </section>
+          <section className="lower">b</section>
+
+        </section>
       </section>
     </div>
   );
