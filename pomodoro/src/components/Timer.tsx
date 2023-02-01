2fbefbbb523f087f4ae4b908ab087249698c56a7
fernando <fernandoreal.valentim@gmail.com>
2023-02-01 10:49:53 -0300

    timer fix

--- pomodoro/src/components/Timer.tsx
+++ pomodoro/src/components/Timer.tsx
@@ -2,59 +2,33 @@ import React, { useState, useEffect } from "react";
 import { CircularProgressbar } from 'react-circular-progressbar';
 
 interface Props {
-  time: number;
-  onReset: () => void;
+  timeSec: number;
+  stopped: boolean;
 }
 
-export const Timer = ({ time, onReset }: Props) => {
-  const [minutes, setMinutes] = useState(Math.floor(time / 60));
-  const [seconds, setSeconds] = useState(time % 60);
-  const [stopped, setStopped] = useState(true);
- 
+export const Timer = ({ timeSec, stopped }: Props) => {
 
-  let interval: NodeJS.Timeout;
+  const [minutes, setMinutes] = useState(Math.floor(timeSec / 60));
+  const [seconds, setSeconds] = useState(timeSec % 60); 
 
   useEffect(() => {
     if (!stopped && !(minutes === 0 && seconds === 0)) {
-      interval = setInterval(() => {
-        if (seconds > 0) {
-          setSeconds(seconds - 1);
-        } else if (minutes > 0) {
-          setMinutes(minutes - 1);
-          setSeconds(59);
-        }
-      }, 1000);
-    } else {
-      clearInterval(interval);
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      } else if (minutes > 0) {
+        setMinutes(minutes - 1);
+        setSeconds(59);
+      }
     }
-    return () => clearInterval(interval);
-  }, [minutes, seconds, stopped]);
 
-  const handleReset = () => {
-    clearInterval(interval);
-    setMinutes(Math.floor(time / 60));
-    setSeconds(time % 60);
-    setStopped(true);
-    onReset();
-  };
-
-  const handleStop = () => {
-    setStopped(!stopped);
-  };
-
+  }, [timeSec])
+  
   return (
     <div>
       <h2 className="timer">
       {/* <CircularProgressbar value={seconds} maxValue={60} text={(minutes < 10 ? `0${minutes}` : minutes) + ":" + (seconds < 10 ? `0${seconds}` : seconds) } /> */}
-      {minutes < 10 ? `0${minutes}` : minutes} :  {seconds < 10 ? `0${seconds}` : seconds} 
-
-      </h2>
-      <button className="stop-button" onClick={handleStop}>
-        {stopped ? "Start" : "Stop"}
-      </button>
-      <button className="reset-button" onClick={handleReset}>
-        Reset
-      </button>
+      {minutes < 10 ? `0${minutes}` : minutes} : {seconds < 10 ? `0${seconds}` : seconds}    
+      </h2>      
     </div>
   );
 };
--- pomodoro/src/components/TimerContainer.tsx
+++ pomodoro/src/components/TimerContainer.tsx
@@ -1,14 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Timer } from "./Timer";
 
 const TimerControls = () => {
-  const [time, setTime] = useState(2340);
+  const [time, setTime] = useState(5);
+  const [stopped, setStopped] = useState(true);
+  
+  let interval: NodeJS.Timeout;
+
+  useEffect(() => {
+    if(!stopped && (time !== 0)) {
+      interval = setTimeout(() => setTime(time - 1) , 1000)
+      console.log(time)
+    } else {
+      clearInterval(interval)
+    }
+  }, [time,stopped])
+
+  
+  const handleReset = () => {
+    clearInterval(interval);
+    setStopped(true);
+    setTime(60);
+  };
+
+  const handleStop = () => {
+    setStopped(!stopped);
+  };
 
 
   return (
     <div>
-      <Timer time={time} onReset={() => setTime(2340)} />
+      <Timer timeSec={time} stopped={stopped} />
 
+      <button className="stop-button" onClick={handleStop}>
+        {stopped ? "Start" : "Stop"}
+      </button>
+      <button className="reset-button" onClick={handleReset}>
+        Reset
+      </button>
       
     </div>
   );
